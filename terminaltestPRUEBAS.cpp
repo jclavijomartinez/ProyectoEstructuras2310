051b9ae84f4cf8a437342b615efd74695a2f3c15
Juan Clavijo <jclavijomartinez@gmail.com>
2023-04-06 18:21:17 -0500

    main basico

--- terminaltestPRUEBAS.cpp
+++ terminaltestPRUEBAS.cpp
@@ -1,33 +1,30 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-#include "movimientos.h"
-#include "comandos.h"
-#include "analisis.h"
-#include "elemento.h"
-
+// #include "movimientos.h"
+// #include "comandos.h"
+// #include "analisis.h"
+// #include "elemento.h"
 using namespace std;
 
-constexpr unsigned int str2int(const char* str, int h = 0)
+void imprimirSimbolo()
 {
-    return !str[h] ? 5381 : (str2int(str, h+1) * 33) ^ str[h];
-}
-
-void imprimirSimbolo() {
   cout << "$ ";
 }
 
-//Prueba
-
-
-int main() {
-  std::string input;
-    
-    comandos elComando;
-    movimientos elMov;
-    analisis elAnal;
-    elemento elElem;
-
-
+int main()
+{
+  // comandos elComando;
+  // movimientos elMov;
+  // analisis elAnal;
+  // elemento elElem;
+  string input;
+  while (true)
+  {
+    imprimirSimbolo();
+    getline(cin, input);
+    if (input == "salir")
+      break;
+  }
   return 0;
-}//Cierre Main
+} // Cierre Main
