1b0c30e08db2bf359774a4867134a1b95f80cffb
Juan Clavijo <jclavijomartinez@gmail.com>
2023-04-06 10:03:42 -0500

    comandos .h y .cpp casi listos, falta llevar a memoria los comandos

--- comandos.cpp
+++ comandos.cpp
@@ -1,31 +1,31 @@
 #include "comandos.h"
 #include <iostream>
-#include <list>
-#include <string>
-#include <sstream>
+#include <fstream>
 
 using namespace std;
-/*
-comandos comandos::consstructor(std::string comando){
 
-    this->comando=comando;
-
-}
-
-*/
-
-void cargar_comando ( string input ) {
-	
-    string nombre_archivo = input.substr(15);
-                cout << "Opción elegida: cargar_comandos" << endl;
-                cout << "Nombre de archivo: " << nombre_archivo << endl;
-	
-
-   // return comandos;
-
-
-}
-
-std::string comandos::getComando() const {
-    return this->comando;
-}
+void cargarComandos(string ruta){
+    fstream newfile;
+    newfile.open(ruta, ios::in);
+    if (newfile.peek() == ifstream::traits_type::eof())
+    {
+        cout << ruta << " no contiene elementos (esta vacio)"<<endl;
+    }
+    else if (newfile.is_open())
+    {
+        string infoarch;
+        int contador_comandos = 0;
+        while (getline(newfile, infoarch))
+        {
+            //cout << infoarch << "\n";
+            contador_comandos++;
+            //la funcion que lleva esta info a memoria
+        }
+        cout << contador_comandos << " comandos cargados correctamente desde " << ruta <<endl;
+        newfile.close();
+    }
+    else
+    {
+        cout << ruta << " no se encuentra o no puede leerse."<<endl;
+    }
+};
--- comandos.h
+++ comandos.h
@@ -2,18 +2,14 @@
 #define COMANDOS_H
 
 #include <string>
-
 class comandos {
 private:
-    std::string comando ;
-
+    std::string ruta;
 public:
-   //comandos consstructor(std::string comando);
-    
-   void cargar_comando(std::string input); //AGREGAR COMANDO
-
-    //Getters y Setters
-    std::string getComando() const;
+    comandos(){}
+    //comandos(string ruta){};
+    ~comandos(){}
+    void cargarComandos(string ruta);
 };
 
 #endif
