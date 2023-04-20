80dbed59012f677e0c97b5e762fbb130bc775f05
Juan Clavijo <jclavijomartinez@gmail.com>
2023-04-19 20:04:22 -0500

    funciona solo cambio jorgeelcurioso

--- terminaltestPRUEBAS.cpp
+++ terminaltestPRUEBAS.cpp
@@ -29,7 +29,7 @@ int main()
 
   string input;
   string funcion;
-  curiosity nave1;
+  curiosity jorgeElCurioso;
   while (true)
   {
     // Conseguir la linea de comandos
@@ -42,8 +42,8 @@ int main()
     // Ahora con el función entrar a un Switch o a puros if´s
     if (funcion == "agregar_movimiento")
     {
-      //nave1.testfun();
-      nave1.agregar_movimiento(input);
+      jorgeElCurioso.testfun();
+      //jorgeElCurioso.agregar_movimiento(input);
     }
     if (funcion == "agregar_analisis")
     {
