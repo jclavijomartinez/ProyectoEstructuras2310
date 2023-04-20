0501603b364dd070c912448aa9403b1731ae922a
Juan Clavijo <jclavijomartinez@gmail.com>
2023-04-20 00:50:26 -0500

    funcion de ayuda completa

--- curiosity.cpp
+++ curiosity.cpp
@@ -311,9 +311,9 @@ void curiosity::ayuda(std::string input)
   {
     cout << "Estos son los comandos disponibles, si necesitas mas ayuda, no dudes en usar ayuda [Comando] " << endl;
     cout << "" << endl;
-    cout << "cargar_elementos [ruta]: Carga los elementos desde el archivo ubicado en la ruta especificada." << endl;
-    cout << "" << endl;
     cout << "cargar_comandos [ruta]: Carga los comandos de movimiento y analisis desde el archivo ubicado en la ruta especificada." << endl;
+    cout << "" << endl;
+    cout << "cargar_elementos [ruta]: Carga los elementos desde el archivo ubicado en la ruta especificada." << endl;
     cout << "" << endl;
     cout << "agregar_movimiento [tipo_mov] [magnitud] [unidad_med]: Agrega el comando de movimiento a la lista de comandos, siendo el movimiento de dos tipos: avanzar o girar, con su respectiva magnitud y unidad de medida. Si la informacion del comando no es completa o adecuada, se muestra un mensaje de error." << endl;
     cout << "" << endl;
@@ -327,42 +327,145 @@ void curiosity::ayuda(std::string input)
     cout << "" << endl;
     cout << "salir: Termina la ejecucion de la aplicacion." << endl;
   }
-  else if (funcdeayuda == "cargar_elementos")
+  else if (funcdeayuda == "cargar_comandos")
   {
-    cout << "Descripción:" << endl;
-    cout << "Carga en memoria los comandos de desplazamiento contenidos en el archivo identificado por nombre_archivo, sobrescribiendo cualquier información previamente cargada en memoria" << endl;
+    cout << "Descripcion:" << endl;
+    cout << "Carga en memoria los comandos de desplazamiento contenidos en el archivo identificado por nombre_archivo, sobrescribiendo cualquier informacion previamente cargada en memoria" << endl;
+    cout << "" << endl;
     cout << "Uso" << endl;
     cout << "    - cargar_comandos [nombre_archivo]" << endl;
-    cout << "Parámetros" << endl;
+    cout << "Parametros" << endl;
     cout << "    - [nombre_archivo]: el nombre o ruta del archivo que contiene los comandos de desplazamiento" << endl;
     cout << "Salida en pantalla" << endl;
-    cout << "    - Si el archivo está vacío: [nombre_archivo] no contiene elementos" << endl;
+    cout << "    - Si el archivo esta vacio: [nombre_archivo] no contiene elementos" << endl;
     cout << "    - Si no se puede abrir el archivo: [nombre_archivo] no se encuentra o no puede leerse" << endl;
     cout << "    - Si se puede abrir el archivo exitosamente y cargar los comandos: n comandos cargados correctamente desde [nombre_archivo]" << endl;
     cout << "Ejemplos de uso" << endl;
     cout << "    - cargar_comandos comandos.csv" << endl;
     cout << "    - cargar_comandos C:/Documentos/comandos.csv" << endl;
   }
-  else if (funcdeayuda == "")
+  else if (funcdeayuda == "cargar_elementos")
   {
+    cout << "Descripcion:" << endl;
+    cout << "La funcion cargar_elementos carga en memoria los datos de puntos de interes o elementos contenidos en el archivo identificado por [nombre_archivo]. " << endl;
+    cout << "Si dentro de la misma sesion de trabajo ya se han cargado otros archivos de puntos de interes, la informacion se sobreescribira en memoria." << endl;
+    cout << "" << endl;
+    cout << "Uso:" << endl;
+    cout << "    cargar_elementos [nombre_archivo]" << endl;
+    cout << "Parametros:" << endl;
+    cout << "    [nombre_archivo]: nombre del archivo que contiene la informacion de los elementos a cargar en memoria (puede ser un .txt o .csv)" << endl;
+    cout << "Salida en pantalla:" << endl;
+    cout << "    - Si el archivo esta vacio: [nombre_archivo] no contiene elementos." << endl;
+    cout << "    - Si no se puede abrir el archivo: [nombre_archivo] no se encuentra o no puede leerse." << endl;
+    cout << "    - Si los comandos se cargan correctamente: n elementos cargados correctamente desde [nombre_archivo]." << endl;
+    cout << "Ejemplo de uso:" << endl;
+    cout << "    - cargar_elementos elementos.txt" << endl;
   }
-  else if (funcdeayuda == "")
+  else if (funcdeayuda == "agregar_movimiento")
   {
+    cout << "Descripcion:" << endl;
+    cout << "La funcion  agregar_movimiento  agrega un comando de movimiento a la lista de comandos del robot  Curiosity." << endl;
+    cout << "El movimiento puede ser de dos tipos:  avanzar  o  girar. " << endl;
+    cout << "La magnitud corresponde al valor del movimiento; si es avanzar, el numero de unidades que se espera avanzar, si es girar la cantidad de grados que debe girar. " << endl;
+    cout << "La unidad de medida corresponde a la convencion con la que se mide la magnitud del movimiento (centimetros, metros, grados...). " << endl;
+    cout << "Si no se envia la informacion completa y adecuada del comando de movimiento, este no puede agregarse a la lista de los comandos que se enviaran al robot desde la tierra." << endl;
+    cout << "Uso:" << endl;
+    cout << "    - agregar_movimiento tipo_mov magnitud unidad_med" << endl;
+    cout << "Parametros:" << endl;
+    cout << "    - tipo_mov: tipo de movimiento, puede ser avanzar o girar." << endl;
+    cout << "    - magnitud: magnitud del movimiento, un numero entero o decimal." << endl;
+    cout << "    - unidad_med: unidad de medida de la magnitud, una cadena de caracteres." << endl;
+    cout << "Salida en pantalla:" << endl;
+    cout << "    - Si hay un formato erroneo: La informacion del movimiento no corresponde a los datos esperados (tipo, magnitud, unidad)." << endl;
+    cout << "    - Si se agrega el comando de movimiento exitosamente: El comando de movimiento ha sido agregado exitosamente." << endl;
+    cout << "Ejemplos de uso:" << endl;
+    cout << "    - Para agregar un movimiento de avanzar 10 metros:" << endl;
+    cout << "        * agregar_movimiento avanzar 10 metros" << endl;
+    cout << "    - Para agregar un movimiento de girar 90 grados:" << endl;
+    cout << "        * agregar_movimiento girar 90 grados" << endl;
   }
-  else if (funcdeayuda == "")
+  else if (funcdeayuda == "agregar_analisis")
   {
+    cout << "Descripcion:" << endl;
+    cout << "Agrega el comando de analisis descrito a la lista de comandos del robot “Curiosity”. " << endl;
+    cout << "El analisis puede ser de tres tipos: fotografiar, composicion o perforar. " << endl;
+    cout << "El objeto es el nombre del elemento que se quiere analizar (roca, arena, monticulo, ...). " << endl;
+    cout << "El comentario es opcional y permite agregar mas informacion sobre el elemento o el analisis, este comentario estara encerrado entre comillas simples (ejemplo: 'roca_cuadrante_32')." << endl;
+    cout << "Uso:" << endl;
+    cout << "    - agregar_analisis tipo_analisis objeto comentario" << endl;
+    cout << "Parametros:" << endl;
+    cout << "    - tipo_analisis: tipo de analisis que se quiere realizar. Puede ser  fotografiar, composicion o perforar." << endl;
+    cout << "    - objeto: nombre del elemento que se quiere analizar (roca, arena, monticulo, etc.)." << endl;
+    cout << "    - comentario (opcional): informacion adicional sobre el elemento o el analisis. Se debe encerrar entre comillas simples." << endl;
+    cout << "Salida en pantalla:" << endl;
+    cout << "    - Si el formato es erroneo: La informacion del analisis no corresponde a los datos esperados (tipo, objeto, comentario)." << endl;
+    cout << "    - Si se agrega el comando de analisis exitosamente: El comando de analisis ha sido agregado exitosamente." << endl;
+    cout << "Ejemplos de uso:" << endl;
+    cout << "    - agregar_analisis fotografiar roca_cuadrante_32  Analizar la textura de la roca " << endl;
+    cout << "    - agregar_analisis perforar monticulo  Tomar muestras para analisis quimico " << endl;
+    cout << "    - agregar_analisis composicion arena  Realizar analisis de elementos en la arena " << endl;
   }
-  else if (funcdeayuda == "")
+  else if (funcdeayuda == "agregar_elemento")
   {
+    cout << "Descripcion:" << endl;
+    cout << "La funcion  agregar_elemento  agrega el componente o elemento descrito a la lista de puntos de interes del robot Curiosity. " << endl;
+    cout << "El tipo de componente puede ser uno entre roca, crater, monticulo o duna. " << endl;
+    cout << "El tamanio es un valor real que da cuenta de que tan grande es el elemento; y la unidad de medida complementa este valor con la convencion que se uso para su medicion (centimetros, metros...). " << endl;
+    cout << "Finalmente, las coordenadas x y y corresponden a numeros reales que permiten conocer la ubicacion del elemento en el sistema de coordenadas del suelo marciano utilizado por el vehiculo." << endl;
+    cout << "Uso:" << endl;
+    cout << " - agregar_elemento tipo_comp tamanio unidad_med coordX coordY" << endl;
+    cout << "Parametros:" << endl;
+    cout << "    - tipo_comp: tipo de componente (roca, crater, monticulo o duna)" << endl;
+    cout << "    - tamanio: tamanio del elemento (valor real)" << endl;
+    cout << "    - unidad_med: unidad de medida del tamanio (centimetros, metros, ...)" << endl;
+    cout << "    - coordX: coordenada X del elemento (valor real)" << endl;
+    cout << "    - coordY: coordenada Y del elemento (valor real)" << endl;
+    cout << "Salida en pantalla:" << endl;
+    cout << "    - Si hay un formato erroneo: La informacion del elemento no corresponde a los datos esperados (tipo, tamanio, unidad, x, y)." << endl;
+    cout << "    - Si se agrega el elemento exitosamente: El elemento ha sido agregado exitosamente." << endl;
+    cout << "Ejemplos de uso:" << endl;
+    cout << "    - agregar_elemento roca 12.5 metros 23.4 45.6" << endl;
+    cout << "    - agregar_elemento crater 8.2 centimetros -12.3 6.7" << endl;
+    cout << "    - agregar_elemento monticulo 5.0 metros 0.0 0.0" << endl;
   }
-  else if (funcdeayuda == "")
+  else if (funcdeayuda == "guardar")
   {
+    cout << "Descripcion:" << endl;
+    cout << "Guarda en un archivo de texto (.txt) la informacion solicitada de acuerdo al tipo de archivo. " << endl;
+    cout << "Los tipos de archivo disponibles son: comandos, que almacena la informacion de comandos de movimiento y de analisis que debe ejecutar el robot" << endl;
+    cout << "y elementos, que almacena la informacion de los componentes o puntos de interes conocidos en el suelo marciano." << endl;
+    cout << "Uso:" << endl;
+    cout << "    - guardar tipo_archivo nombre_archivo" << endl;
+    cout << "Parametros:" << endl;
+    cout << "    - tipo_archivo: tipo de archivo a guardar (comandos o elementos)." << endl;
+    cout << "    - nombre_archivo: nombre del archivo en el que se guardara la informacion. Debe tener la extension .txt." << endl;
+    cout << "Salida en pantalla:" << endl;
+    cout << "- Si no hay informacion: La informacion requerida no esta almacenada en memoria." << endl;
+    cout << "- Si la escritura es exitosa: La informacion ha sido guardada en nombre_archivo." << endl;
+    cout << "- Si hay problemas al guardar el archivo: Error guardando en nombre_archivo." << endl;
+    cout << "Ejemplo de uso:" << endl;
+    cout << "    guardar comandos comandos.txt" << endl;
   }
-  else if (funcdeayuda == "")
+  else if (funcdeayuda == "simular")
   {
+    cout << "Descripcion:" << endl;
+    cout << "Permite simular el resultado de los comandos de movimiento que se enviaran al robot  Curiosity  desde la Tierra, facilitando asi la validacion de la nueva posicion en la que podria ubicarse. " << endl;
+    cout << "Para ejecutarse adecuadamente, requiere conocer la posicion actual (coordenadas X y Y) del vehiculo. " << endl;
+    cout << "Hay que tener en cuenta que solo los comandos de movimiento son necesarios, no los de analisis." << endl;
+    cout << "Uso:" << endl;
+    cout << "    simular_comandos coordX coordY" << endl;
+    cout << "Parametros:" << endl;
+    cout << "    - coordX: coordenada X actual del vehiculo." << endl;
+    cout << "    - coordY: coordenada Y actual del vehiculo." << endl;
+    cout << "Salida en pantalla:" << endl;
+    cout << "    - Si no se tiene la informacion necesaria: La informacion requerida no esta almacenada en memoria." << endl;
+    cout << "    - Si la simulacion es exitosa: La simulacion de los comandos, a partir de la posicion (coordX,coordY), deja al robot en la nueva posicion (nuevoX,nuevoY)." << endl;
+    cout << "Ejemplo de uso:" << endl;
+    cout << "    simular_comandos 10 5" << endl;
   }
-  else if (funcdeayuda == "")
+  else if (funcdeayuda == "salir")
   {
+    cout << "Termina la ejecucion de la aplicacion y regresa a la terminal de windows." << endl;
   }
   else
   {
